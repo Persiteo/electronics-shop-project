be2cc4636fae4dcdc2482dd14bd363876d522d6e
Persiteo <Persiteo@users.noreply.github.com>
2023-06-18 23:29:50 +0300

    add tests

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import os
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +17,22 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
+
+    @property
+    def item_name(self):
+        return self.__name
+
+    @item_name.setter
+    def item_name(self, name):
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            self.__name = name[0:10]
+            raise Exception('Длина наименования товара должна быть не больше 10 символов')
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +47,26 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+        Класс-метод, инициализирующий экземпляры класса `Item` данными из файла _src/items.csv
+        """
+        cls.all = []
+        with open(os.path.join(os.path.dirname(__file__), 'items.csv'), newline='', encoding='cp1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for each_row in reader:
+                name = each_row['name']
+                price = each_row['price']
+                quantity = each_row['quantity']
+                cls(name, float(price), int(quantity))
+
+
+    @staticmethod
+    def string_to_number(value):
+        """
+        Метод, возвращающий число из числа-строки
+        """
+        number = int(float(value))
+        return number
--- tests/test_item.py
+++ tests/test_item.py
@@ -21,3 +21,30 @@ def test_calculate_total_price(some_item):
 
 def test_apply_discount(some_item):
     assert some_item.apply_discount() is None
+
+def test_item_name():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("СуперСмартфон", 10000, 20)
+    assert item1.name == "Смартфон"
+    assert item2.name == "СуперСмартфон"
+    assert print(item2.name) == print("Наименования товара должно быть не больше 10 симвовов")
+
+
+def test_instantiate_from_csv():
+    items = Item.instantiate_from_csv()
+    assert len(items) == 5
+
+    item1 = items[0]
+    assert item1.name == 'Смартфон'
+    assert item1.price == 100
+    assert item1.quantity == 1
+
+
+def test_string_to_number():
+    Item.instantiate_from_csv()
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
